d67e5d033e083ba7065d5e4bafca428e62ebfe81
Aragon Joaquin <aragonjoaquin05@gmail.com>
2025-11-13 10:20:32 -0300

    more fixes

--- frontend/src/components/GCgenerics/index.ts
+++ frontend/src/components/GCgenerics/index.ts
@@ -2,17 +2,9 @@
 export * from './Button'
 export * from './CoolDivider'
 export * from './ElementSlider'
+export * from './GCList'
 export * from './Header'
 export * from './Input'
 export * from './ScrollbarSlider'
 export * from './SearchBar'
-<<<<<<< HEAD
-<<<<<<< HEAD
 export * from './Skeleton'
-=======
-export * from './GCList'
->>>>>>> origin/feature/dashboard_ui
-=======
-export * from './GCList'
-export * from './Skeleton'
->>>>>>> origin/feature/landing
