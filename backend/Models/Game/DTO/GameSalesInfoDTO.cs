d3e8c1f69a7bcadb5f3c30a5d6c96f59c1c3db53
fede-vazquez <fedenv2000@gmail.com>
2025-11-13 01:21:47 -0300

    fix: Agrega GameSalesInfo.

---
+++ backend/Models/Game/DTO/GameSalesInfoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace GameCore.Models.Game.DTO
+{
+    public class GameSalesInfo
+    {
+        public int GameId { get; set; }
+        public string Title { get; set; } = null!;
+        public decimal TotalSales { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
