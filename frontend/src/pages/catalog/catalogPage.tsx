37f8fd10dd50878b7e7f3d73aec67e5ad9c0a9b3
Aragon Joaquin <aragonjoaquin05@gmail.com>
2025-11-14 11:01:26 -0300

    loading button

--- frontend/src/pages/catalog/catalogPage.tsx
+++ frontend/src/pages/catalog/catalogPage.tsx
@@ -1,21 +1,27 @@
 import { ThrobberSVG } from '@/assets'
 import { HorizontalCard } from '@/components/game'
-import { GCDivider, GCSearchBar } from '@/components/GCgenerics'
+import { GCButton, GCDivider, GCSearchBar } from '@/components/GCgenerics'
 import type { GameListResponse, GenreDTO } from '@/models'
 import { makeApiCall } from '@/services/apiCall'
 import { debouncer, QUERY_KEYS } from '@/utils'
 import { useQueries } from '@tanstack/react-query'
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { FilterDropMenu } from './components'
 import { useCatalogContext } from './context'
+
+const INITIAL_PAGE_SIZE = 20 as const
 
 export function CatalogPage() {
 	// i can use the remaining props as well, but this the only IMPORTANT module who uses this
 	const { genres, setGenres, catalogGames, setCatalogGames, getPrevGames } = useCatalogContext()
+	const PAGE_CURRENT = useRef<number>(1)
 
 	//todo: fix this boilerplate oh god
 	const [enabled, setEnabled] = useState<boolean>(true)
 	const [gameEnabled, setGameEnabled] = useState<boolean>(true)
+
+	//view more button. yes this needs improvement
+	const [disabled, setDisabled] = useState<boolean>(false)
 
 	const [{ data, error }, { data: gameData, error: gameError, isPending }] = useQueries({
 		queries: [
@@ -36,7 +42,10 @@ export function CatalogPage() {
 				queryFn: async () => {
 					try {
 						const data = (
-							await makeApiCall<GameListResponse>({ endpoint: '/Games?', opts: { filters: { PageSize: 20 } } })
+							await makeApiCall<GameListResponse>({
+								endpoint: '/Games?',
+								opts: { filters: { PageSize: INITIAL_PAGE_SIZE, PageNumber: PAGE_CURRENT.current, Ascending: 'true' } }
+							})
 						)?.items
 
 						//save prevoius games instead of doing anoither fetch
@@ -94,19 +103,52 @@ export function CatalogPage() {
 				</span>
 				<GCDivider className="translate-y-2! bottom-0!" />
 			</section>
-			<article className="justify-evenly xl:justify-start w-full relative bg-neutral-900 rounded-xl p-2 min-h-[200px] flex flex-wrap gap-3 lg:gap-5">
-				{isPending ? (
-					<ThrobberSVG className="absolute top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 animate-spin h-12 w-fit flex grow" />
-				) : catalogGames?.length ? (
-					catalogGames.map((g) => {
-						return <HorizontalCard game={g} key={g.id} />
-					})
-				) : (
-					<p className="w-full text-center absolute top-1/2 -translate-y-1/2 font-semibold text-xl text-red-400">
-						{gameError?.message ?? 'No games available'}
-					</p>
-				)}
-			</article>
+			<section className="flex flex-col items-center gap-y-5 bg-neutral-900 rounded-xl mb-20">
+				<article className="flex justify-evenly  w-full relative p-2 min-h-[200px] flex-wrap gap-3 lg:gap-5">
+					{isPending ? (
+						<ThrobberSVG className="absolute top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 animate-spin h-12 w-fit flex grow" />
+					) : catalogGames?.length ? (
+						catalogGames.map((g) => {
+							return <HorizontalCard game={g} key={g.id} />
+						})
+					) : (
+						<p className="w-full text-center absolute top-1/2 -translate-y-1/2 font-semibold text-xl text-red-400">
+							{gameError?.message ?? 'No games available'}
+						</p>
+					)}
+				</article>
+				<GCButton
+					theme="primary"
+					className="mb-5"
+					disabled={disabled || !catalogGames.length}
+					onClick={async () => {
+						try {
+							const data = (
+								await makeApiCall<GameListResponse>({
+									endpoint: '/Games?',
+									opts: {
+										filters: {
+											PageSize: INITIAL_PAGE_SIZE,
+											PageNumber: PAGE_CURRENT.current + 1,
+											Ascending: 'true'
+										}
+									}
+								})
+							)?.items
+
+							console.log(data)
+
+							if (!data.length) return setDisabled(true)
+
+							PAGE_CURRENT.current = PAGE_CURRENT.current + 1
+							getPrevGames.current = [...catalogGames, ...data]
+							setCatalogGames((prev) => [...prev, ...data])
+						} catch {}
+					}}
+				>
+					Load MOREE!!!
+				</GCButton>
+			</section>
 		</main>
 	)
 }
